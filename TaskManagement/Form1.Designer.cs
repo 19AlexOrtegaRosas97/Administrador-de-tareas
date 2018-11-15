76251db7949923e36947523a1d90dddd37381139
19AlexOrtega97 <ortega4414@gmail.com>
2018-11-15 16:07:40 -0600

    Task manager

---
+++ TaskManagement/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TaskManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(418, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(43, 22);
+            this.toolStripButton1.Text = "Iniciar";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 325);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(418, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(111, 17);
+            this.toolStripStatusLabel1.Text = "Procesos Actuales:0";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(418, 300);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "NOMBRE";
+            this.columnHeader1.Width = 138;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "MEMORIA";
+            this.columnHeader2.Width = 86;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ACTIVA";
+            this.columnHeader3.Width = 87;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "ID";
+            this.columnHeader4.Width = 98;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(312, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Terminar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(54, 17);
+            this.toolStripStatusLabel2.Text = "Tiempo: ";
+            this.toolStripStatusLabel2.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Parar Tiempo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(418, 347);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Administrado de Tareas CTF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TaskManagement/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace TaskManagement
+{
+    public partial class Form1 : Form
+    {
+        List<Process> listaProcesos = new List<Process>();
+        bool bandera;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ms = 0;
+             s = 0;
+             m = 0;
+            toolStripStatusLabel2.Text = "Tiempo ejecucion: ";
+            bandera = true;
+            actualizarListView();
+            timer1.Start();
+            if (bandera == true)
+            {
+                //toolStripStatusLabel2 = timer1.Interval.ToString();
+            }
+
+
+        }
+
+
+        private void actualizarListView()
+        {
+            int contador=0;
+            listView1.Items.Clear();
+            
+            foreach (Process proceso in Process.GetProcesses())
+            {
+                string[] arregloDatos = new string[4];
+                arregloDatos[0] = proceso.ProcessName;
+                arregloDatos[1] = darFormato((proceso.PrivateMemorySize64/1024).ToString());
+                arregloDatos[2] = proceso.Responding.ToString();
+                //arregloDatos[3] =  num.Next().ToString();
+                arregloDatos[3]= proceso.Id.ToString();
+                ListViewItem item = new ListViewItem(arregloDatos);
+                listView1.Items.Add(item);
+                toolStripStatusLabel1.Text = "Procesos Actuales: " + listView1.Items.Count;
+                contador += 1;
+            }
+        }
+        private string darFormato(string mb)
+        {
+            string mbFinal = "";
+            if(mb.Length <= 4)
+            {
+                int x=0;
+                foreach(char a in mb)
+                {
+                    if (x==1)
+                    {
+                        mbFinal = mbFinal + "." + a;
+                    }
+                    else
+                    {
+                        if(x== (mb.Length - 1)) {
+                            mbFinal = mbFinal + a;
+                            Console.WriteLine(mbFinal);
+                            return mbFinal + " MB";
+                        }
+                        else
+                        {
+                            mbFinal = mbFinal + a;
+                        }
+                        
+                    }
+                    x++;
+                }         
+            }
+
+            if (mb.Length > 4)
+            {
+                int x = 0;
+                foreach (char a in mb)
+                {
+                    if (x == 2)
+                    {
+                        mbFinal = mbFinal + "." + a;
+                    }
+                    else
+                    {
+                        if (x == (mb.Length - 1))
+                        {
+                            mbFinal = mbFinal + a;
+                            Console.WriteLine(mbFinal);
+                            return mbFinal + " MB";
+                        }
+                        else
+                        {
+                            mbFinal = mbFinal + a;
+                        }
+                    }
+                    x++;
+                }
+            }
+            return "00.00 MB";
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            //Console.WriteLine(listView1.FocusedItem.SubItems[4].ToString());
+            //Console.WriteLine(obtenID(listView1.FocusedItem.SubItems[4].ToString()));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idProceso = obtenID(listView1.FocusedItem.SubItems[3].ToString());
+            try
+            {
+                System.Diagnostics.Process.GetProcessById(idProceso).Kill();
+            }
+            catch
+            {
+                actualizarListView();
+            }
+            
+            actualizarListView();
+        }
+
+        private void agregaDatosLista()
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                listaProcesos.Add(p);
+            }
+        }
+
+        private void agregarListaAlaListview()
+        {
+            listaProcesos.Sort();
+        }
+
+        private int obtenID(string palabra)
+        {
+            string num="";
+            foreach(char x in palabra)
+            {
+                if(char.IsDigit(x))
+                {
+                    num=num+x;
+                }
+            }
+            return Convert.ToInt32(num);
+        }
+        int ms=0;
+        int s = 0;
+        int m = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            ms++;
+            if (ms == 100)
+            {
+                if ((s % 5) == 0 && s!=0) { actualizarListView(); }           
+                s++;
+                ms = 0;
+            }
+            else if (s == 60)
+            {
+                m++;
+                s = 0;
+            }
+            toolStripStatusLabel2.Text = "Tiempo ejecucion: " + m.ToString().PadLeft(2, '0') + ":" + s.ToString().PadLeft(2, '0') + ":" + ms.ToString().PadLeft(2, '0');
+          
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
+
